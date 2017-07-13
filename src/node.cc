electron/node

Guard against NULL returned by uv_err_name.
The uv_err_name would return NULL on unknown system error, which then would
crash V8 by creating string from NULL.

src/node.cc
@@ -735,7 +735,11 @@ Local<Value> UVException(int errorno,
   if (!msg || !msg[0])
     msg = uv_strerror(errorno);
 
-  Local<String> estring = OneByteString(node_isolate, uv_err_name(errorno));
+  const char* err_name = uv_err_name(errorno);
+  if (err_name == NULL)
+    err_name = "UnknownSystemError";
+
+  Local<String> estring = OneByteString(node_isolate, err_name);
   Local<String> message = OneByteString(node_isolate, msg);
   Local<String> cons1 =
       String::Concat(estring, FIXED_ONE_BYTE_STRING(node_isolate, ", "));

ApolloRobot/node-1

src/node.cc
@@ -941,7 +941,11 @@ Local<Value> UVException(Isolate* isolate,
   if (!msg || !msg[0])
     msg = uv_strerror(errorno);
 
-  Local<String> js_code = OneByteString(isolate, uv_err_name(errorno));
+  const char* err_name = uv_err_name(errorno);
+  if (err_name == NULL)
+    err_name = "UnknownSystemError";
+
+  Local<String> js_code = OneByteString(isolate, err_name);
   Local<String> js_syscall = OneByteString(isolate, syscall);
   Local<String> js_path;
   Local<String> js_dest;